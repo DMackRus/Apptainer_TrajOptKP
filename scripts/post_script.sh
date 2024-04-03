a3c4fda83e2ed0017c3cb239afa79cf8421ff9ac
DMakcRus <el16dmcr@leeds.ac.uk>
2024-04-03 13:27:47 +0100

    removed clion. Added eigen, yamlreader, libxinerama, libxcursor, libxi to install with root access. Added run script shell to download my forked mujoco, build it so it can be used. Also export MJ_HOME environemtn varibale into bashrc

--- scripts/post_script.sh
+++ scripts/post_script.sh
@@ -24,7 +24,13 @@ apt-get install -y --force-yes \
     mesa-utils \
     software-properties-common \
     libglfw3 \
-    libglfw3-dev
+    libglfw3-dev \	# GLFW3 for visualisation
+    libxinerama-dev \	# MuJoCo build dependancy
+    libxcursor \	# MuJoCo build dependancy
+    libxi \		# MuJoCo build dependancy
+    libeigen3-dev \	# Eigen
+    libyaml-cpp-dev \	# Yaml reader
+    libgtest-dev	# Google test
 
 # Packages and installations of drake
 # Install glibc6
@@ -39,57 +45,11 @@ cd build
 make
 make install
 
-# Installation of ROS Noetic and essential packages
-#sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-#curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
-#apt update
-#apt install -y --force-yes \
-#    ros-noetic-desktop \
-#    ros-noetic-rosbash \
-#    python3-rosdep \
-#    python3-wstool \
-#    python3-catkin-tools
-
-# Init rosdep and update
-#rosdep init
-#rosdep update
-
-# Installation of MoveIt! related packages
-#apt install -y --force-yes \
-#    ros-noetic-libfranka \
-#    ros-noetic-franka-ros \
-#    ros-noetic-panda-moveit-config \
-#    ros-noetic-effort-controllers \
-#    ros-noetic-position-controllers \
-#    ros-noetic-ros-controllers \
-#    ros-noetic-effort-controllers \
-#    ros-noetic-rqt-controller-manager \
-#    ros-noetic-four-wheel-steering-msgs \
-#    ros-noetic-urdf-geometry-parser \
-#    ros-noetic-urdf-geometry-parser \
-#    ros-noetic-rosparam-shortcuts \
-#    ros-noetic-moveit-resources-panda-moveit-config
-
-# Install drake via apt - get
-#apt-get update
-#apt-get install --no-install-recommends \
-#  ca-certificates gnupg lsb-release wget
-
-#wget -qO- https://drake-apt.csail.mit.edu/drake.asc | gpg --dearmor - \
-#  | sudo tee /etc/apt/trusted.gpg.d/drake.gpg >/dev/null
-
-#echo "deb [arch=amd64] https://drake-apt.csail.mit.edu/$(lsb_release -cs) $(lsb_release -cs) main" \
-#  | sudo tee /etc/apt/sources.list.d/drake.list >/dev/null
-
-#apt-get update 
-#apt-get install -y --force-yes --no-install-recommends drake-dev
-# Add the environment variable to PATH via bashrc
-
 # Install clion with wget and unzipping
-wget https://download.jetbrains.com/cpp/CLion-2022.3.3.tar.gz
-mv CLion-2022.3.3.tar.gz /opt 
-cd /opt 
-tar xvzf CLion-2022.3.3.tar.gz
+#wget https://download.jetbrains.com/cpp/CLion-2022.3.3.tar.gz
+#mv CLion-2022.3.3.tar.gz /opt 
+#cd /opt 
+#tar xvzf CLion-2022.3.3.tar.gz
 
 # Install firefox
 apt install -y --force-yes firefox
@@ -99,7 +59,7 @@ apt install -y --force-yes firefox
 CUSTOM_ENV=/.singularity.d/env/99-zz_custom_env.sh
 cat >$CUSTOM_ENV <<EOF
 #!/bin/bash
-PS1="[PandaPlanning_Dave] Singularity> \w \$ "
+PS1="[TrajOptKP_Sing] Singularity> \w \$ "
 EOF
 chmod 755 $CUSTOM_ENV
 
--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -13,7 +13,26 @@ if [ ! -d "$HOME/TrajOptKP" ]; then
     make install 
 
     cd $HOME
+    
+    # Install MuJoCo
+    git clone git@github.com:DMackRus/mujoco.git mujoco_temp
+    cd mujoco_temp
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX="~/mujoco"
+    cmake --install .
+    
+    #Remove temp MuJoCo directory
+    cd $HOME
+    rm -rf /mujoco_temp
+    
+    # Export environment variables to bashrc
+    echo export MJ_HOME='"'$(pwd)/mujoco'"' >> ~/.bashrc
+
+    
 fi
+
+
 
 source $HOME/.bashrc
 alias xterm="xterm -fa 'Monospace' -fs 14 &"
