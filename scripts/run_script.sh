410104d9d33f8dbed7a6923362f2bbeec289db23
DMakcRus <el16dmcr@leeds.ac.uk>
2024-04-03 11:20:54 +0100

    modified run script to delete bloatware packages

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-source /opt/ros/noetic/setup.bash
 
-if [ ! -d "$HOME/catkin_ws/devel" ]; then
-    cd $HOME/catkin_ws/src
-    git clone git@github.com:roboticsleeds/moveit.git; cd moveit; git checkout noetic-devel; cd ..
-    git clone git@github.com:roboticsleeds/moveit_msgs
-    git clone git@github.com:roboticsleeds/panda_moveit_config.git
-
-    git clone git@github.com:DMackRus/mujocoLib.git
-    git clone git@github.com:DMackRus/automaticTOTaskSpecification.git autoTOTask
-    cd $HOME/catkin_ws/src/autoTOTask
-    git clone git@github.com:DMackRus/Franka-emika-panda-arm.git
-
-    cd $HOME/catkin_ws/src/
-
-    git clone git@github.com:DMackRus/drakeTest.git
+if [ ! -d "$HOME/TrajOptKP" ]; then
+    git clone --recursive git@github.com:DMackRus/TrajOptKP.git
 
     #Install yaml-cpp
     git clone git@github.com:jbeder/yaml-cpp.git
@@ -25,17 +12,9 @@ if [ ! -d "$HOME/catkin_ws/devel" ]; then
     make 
     make install 
 
-    cd $HOME/catkin_ws/src/
-
-    git clone git@github.com:roboticsleeds/franka_ros.git
-    cd $HOME/catkin_ws/src/franka_ros
-    git submodule update --init --recursive
-
-    cd $HOME/catkin_ws/
-    catkin_make
+    cd $HOME
 fi
 
-source $HOME/catkin_ws/devel/setup.bash
 source $HOME/.bashrc
 alias xterm="xterm -fa 'Monospace' -fs 14 &"
 
