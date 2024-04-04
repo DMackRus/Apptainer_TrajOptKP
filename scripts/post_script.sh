fee6d8ff336d5d2f0db24cc0a64adbb37dbc9752
DMakcRus <el16dmcr@leeds.ac.uk>
2024-04-04 10:36:38 +0100

    Reverted changes to post and run script.

--- scripts/post_script.sh
+++ scripts/post_script.sh
@@ -5,32 +5,32 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y keyboard-configuration
 DEBIAN_FRONTEND=noninteractive TZ="Europe/London" apt-get install -y tzdata
 apt-get -y upgrade
 
-apt-get install -y --force-yes \
-    wget \
-    vim-gtk \
-    zip \
-    unzip \
-    git \
-    build-essential \
-    software-properties-common \
-    xterm \
-    pypy \
-    cmake \
-    curl \
-    python \
-    psmisc \
-    apt-utils \
-    python3-pip \
-    mesa-utils \
-    software-properties-common \
-    libglfw3 \
-    libglfw3-dev \
-    libxinerama-dev \
-    libxcursor-dev \
-    libxi-dev \
-    libeigen3-dev \
-    libyaml-cpp-dev \
-    libgtest-dev
+#apt-get install -y --force-yes \
+#    wget \
+#    vim-gtk \
+#    zip \
+#    unzip \
+#    git \
+#    build-essential \
+#    software-properties-common \
+#    xterm \
+#    pypy \
+#    cmake \
+#    curl \
+#    python \
+#    psmisc \
+#    apt-utils \
+#    python3-pip \
+#    mesa-utils \
+#    software-properties-common \
+#    libglfw3 \
+#    libglfw3-dev \
+#    libxinerama-dev \
+#    libxcursor-dev \
+#    libxi-dev \
+#    libeigen3-dev \
+#    libyaml-cpp-dev \
+#    libgtest-dev
 
 # Packages and installations of drake
 # Install glibc6
@@ -62,21 +62,4 @@ cat >$CUSTOM_ENV <<EOF
 PS1="[TrajOptKP_apptainer] Singularity> \w \$ "
 EOF
 chmod 755 $CUSTOM_ENV
-
-# Install MuJoCo
-git clone git@github.com:DMackRus/mujoco.git mujoco_temp
-cd mujoco_temp
-mkdir build
-cd build
-cmake ..
-cmake --build .
-cmake .. -DCMAKE_INSTALL_PREFIX="~/mujoco"
-cmake --install .
-
-#Remove temp MuJoCo directory
-cd $HOME
-rm -rf mujoco_temp
-
-# Export environment variables to bashrc
-echo export MJ_HOME='"'$(pwd)/mujoco'"' >> ~/.bashrc
 
--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/TrajOptKP" ]; then
-    #git clone --recursive git@github.com:DMackRus/TrajOptKP.git
+	# Install MuJoCo
+	git clone git@github.com:DMackRus/mujoco.git mujoco_temp
+	cd mujoco_temp
+	mkdir build
+	cd build
+	cmake ..
+	cmake --build .
+	cmake .. -DCMAKE_INSTALL_PREFIX="~/mujoco"
+	cmake --install .
 
-    cd $HOME
+	#Remove temp MuJoCo directory
+	cd $HOME
+	rm -rf mujoco_temp
+
+	# Export environment variables to bashrc
+	echo export MJ_HOME='"'$(pwd)/mujoco'"' >> ~/.bashrc
+
+    	git clone --recursive git@github.com:DMackRus/TrajOptKP.git
+
+    	cd $HOME
     
 fi
 
