2f9c5d0dbbf600e5c58ceec0208754cccab3b65f
DMackRus <el16dmcr@leeds.ac.uk>
2024-11-01 12:26:33 +0000

    Fixed a silly mistake, i wasnt passing in the GPU to my container in the run script

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ rm -rf workdir/*
 
 singularity run \
     --contain \
+    $(if lspci | grep -qi nvidia; then echo ' --nv'; else echo ''; fi) \
     $(if [ -f "${HOME}/.gitconfig" ]; then echo ' --bind='${HOME}'/.gitconfig'; else echo ''; fi) \
     $(if [ -f "${HOME}/.vimrc" ]; then echo ' --bind='${HOME}'/.vimrc'; else echo ''; fi) \
     --bind=/etc/hosts \
