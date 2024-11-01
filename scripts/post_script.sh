e9b94ecbd5b62541fba84f7b931d1cc1b750d190
DMackRus <el16dmcr@leeds.ac.uk>
2024-11-01 12:29:59 +0000

    Upgrading ubuntu version in contaienr to latest to hopefully get around glibc versiopn error

--- scripts/post_script.sh
+++ scripts/post_script.sh
@@ -33,19 +33,6 @@ apt-get install -y --force-yes \
     libgtest-dev \
     libpng-dev
 
-# Packages and installations of drake
-# Install glibc6
-apt-get install -y --force-yes gawk bison
-cd /opt
-git clone https://sourceware.org/git/glibc.git glibc-source
-cd glibc-source
-git checkout release/2.34/master
-mkdir build
-cd build
-../configure --prefix=/opt/glibc
-make
-make install
-
 # Install clion with wget and unzipping
 wget https://download.jetbrains.com/cpp/CLion-2022.3.3.tar.gz
 mv CLion-2022.3.3.tar.gz /opt 
@@ -53,14 +40,14 @@ cd /opt
 tar xvzf CLion-2022.3.3.tar.gz
 
 # Install firefox
-apt install -y --force-yes firefox
+#apt install -y --force-yes firefox
 
 # Install CUDA-12.4
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-ubuntu2004-12-4-local_12.4.0-550.54.14-1_amd64.deb
-dpkg -i cuda-repo-ubuntu2004-12-4-local_12.4.0-550.54.14-1_amd64.deb
-cp /var/cuda-repo-ubuntu2004-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-ubuntu2204-12-4-local_12.4.0-550.54.14-1_amd64.deb
+dpkg -i cuda-repo-ubuntu2204-12-4-local_12.4.0-550.54.14-1_amd64.deb
+cp /var/cuda-repo-ubuntu2204-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
 apt-get update
 apt-get -y install cuda-toolkit-12-4
 
